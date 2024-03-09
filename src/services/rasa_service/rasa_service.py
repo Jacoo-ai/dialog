9e33097f3f497eae22a7df563e5f9227a48992d9
7z1m15c3 <1016251103@qq.com>
2024-03-09 21:25:32 +0000

    latest model for story 3

--- src/services/rasa_service/rasa_service.py
+++ src/services/rasa_service/rasa_service.py
@@ -25,7 +25,7 @@ class Rasa:
         return text
 
 
-model_path = "rasa_train/models/20240309-020055-plain-bag.tar.gz"
+model_path = "rasa_train/models/20240309-212349-quiet-shadow.tar.gz"
 # model_path = "rasa_train/models/20240305-012050-cool-persian.tar.gz"
 # model_path = "rasa_train/models/20240213-212046-largo-twitch.tar.gz"
 print("rasa server started...")
